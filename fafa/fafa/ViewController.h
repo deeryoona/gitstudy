74355d25296ee274c55cf7b71132073ac58ce828
macmini <764665007@qq.com>
2022-08-02 18:06:07 +0800

    fafaf

---
+++ fafa/fafa/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  fafa
+//
+//  Created by macmini on 2022/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fafa/fafa/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  fafa
+//
+//  Created by macmini on 2022/8/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"afafaf");
+}
+
+
+@end
